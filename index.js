b0a2b690269670b9cfe43d93497619c8baf172c4
Sophie Gu <sophieqgu@gmail.com>
2021-01-20 03:13:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Your code here
 function saturdayFun(something = "roller-skate") {
-  return "This saturday, I want to " + something + "!";
+  return "This Saturday, I want to " + something + "!";
+}
+
+function mondayWork(something = "go to the office") {
+  return "This Monday, I will " + something + ".";
 }
