01847779da7195b465f2407a55f57c184cedf6b6
Sophie Gu <sophieqgu@gmail.com>
2021-01-20 04:06:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // Your code here
 function saturdayFun(something = "roller-skate") {
-  return "This Saturday, I want to " + something + "!";
+  return `This Saturday, I want to ${something}!`;
 }
 
 function mondayWork(something = "go to the office") {
-  return "This Monday, I will " + something + ".";
+  return `This Monday, I will ${something}.`;
 }
 
 function wrapAdjective {
-  return function(very = "*") {
+  return function(emphasis = "*") {
     return function(something = "special") {
-      return "You are " + very + something + very + "!";
+      return `You are ${emphasis} ${something} ${emphasis}!`;
     }
   }
 }
